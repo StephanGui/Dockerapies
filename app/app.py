c93e5da085469cf37d25b36af2264e376139c914
stephan.gui <stephan.gui4@gmail.com>
2020-05-20 15:23:28 +0200

    docker runt nu goei zonder connectie met mongo db

--- app/app.py
+++ app/app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 import pymongo
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def hello_world():
-
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
